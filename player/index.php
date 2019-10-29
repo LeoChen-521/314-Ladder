94939ea8384e92e61f46870961be44e795a49748
LeoChen <xchen13@georgefox.edu>
2019-10-29 22:33:44 +0000

    added player file

--- player/index.php
+++ player/index.php
@@ -4,15 +4,15 @@ include_once("../rest.php");
 $request = new RestRequest();
 $vars = $request->getRequestVariables();
 
-$db = new PDO("pgsql:dbname=ladder_xchen13 host=localhost password=1846485 user=xchen13");
-//$db = new PDO("pgsql:dbname=ladder host=localhost password=314dev user=dev");
+//$db = new PDO("pgsql:dbname=ladder_xchen13 host=localhost password=1846485 user=xchen13");
+$db = new PDO("pgsql:dbname=ladder host=localhost password=314dev user=dev");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $results = array();
 
 if($request->isGet())
 {
-	if (count($vars) !== 1)
+	if (!array_key_exists("username", $vars))
 	{
 		http_response_code(400);
 	}
@@ -21,13 +21,15 @@ if($request->isGet())
 		$username = $vars["username"];
 		http_response_code(200);
 
+		//$check_results = username_exists($db, $username);
+
 		$username_check_sql = "select count(*) from player where username = ?";
 		$statement_check = $db->prepare($username_check_sql);
 		$statement_check->execute([$username]);
 		$check_results_array = $statement_check->fetch(PDO::FETCH_ASSOC);
 		$check_results = intval($check_results_array["count"]);
 
-		if($check_results !== 1)
+		if($check_results === 0)
 		{
 			$results = array("player" => "");	
 		}
@@ -98,83 +100,146 @@ if($request->isGet())
 elseif($request->isPost())
 {
 	// Check missing any inputs
-	$input_array_length = count($vars);
+	//$input_array_length = count($vars);
 
-	if($input_array_length === 5)
+	//if($input_array_length === 5)
+	$has_error = false;
+
+	//Check the inputs given or not.
+	// name
+	if(array_key_exists("name", $vars))
 	{
 		$name = $vars["name"];
-		$email = $vars["email"];
-		$phone = $vars["phone"];
-		$username = $vars["username"];
-		$password = $vars["password"];
+	}
+	else
+	{
+		$results = array("error_text" => "No name given");
+		$has_error = true;
+	}
 
-		if(!preg_match("/^[0-9]{10}+$/", $phone))
-		{
-			$results = array("error_text" => "Invalid phone number.");
-			http_response_code(400);
-		}
+	//validate email 
+	if(array_key_exists("email", $vars))
+	{
+		$email = $vars["email"];
 		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
 		{
 			$results = array("error_text" => "Invalid email address.");
 			http_response_code(400);
 		}
-		else
+	}
+	else
+	{
+		$results = array("error_text" => "No email given");
+		$has_error = true;
+	}
+
+
+	//validate phone number
+	if(array_key_exists("phone", $vars))
+	{
+		$phone = $vars["phone"];
+		if(!preg_match("/^[0-9]{10}+$/", $phone))
 		{
-			//unique username
-			$username_check_sql = "select count(*) from player where username = ?";
-			$statement_check_username = $db->prepare($username_check_sql);
-			$statement_check_username->execute([$username]);
-			$check_username_results_array = $statement_check_username->fetch(PDO::FETCH_ASSOC);
-			$check_username_results = intval($check_username_results_array["count"]);
-
-			//if the username is already exist, error text.
-			if($check_username_results !== 0)
-			{
-				$results = array("error_text" => "This username has been used.");
-				http_response_code(400);
-			}
-
-			try
-			{
-				$db->beginTransaction();
-
-				$highest_rank_sql = "select max(rank) from player";
-				$statement_highest_rank = $db->prepare($highest_rank_sql);
-				$statement_highest_rank->execute();
-				$highest_rank_result = $statement_highest_rank->fetch(PDO::FETCH_ASSOC);
-
-				$current_rank = $highest_rank_result["max"] + 1;
-
-
-				$sql = "insert into player (name, email, rank, phone, username, password) values (?, ?, ?, ?, ?, ?)";
-				$statement = $db->prepare($sql);
-				$statement->execute([$name, $email, $current_rank, $phone, $username, $password]);
-
-				$db->commit();
-
-				$results = array("error_text" => "");
-				http_response_code(202);
-			}
-			catch(PDOException $e)//400
-			{
-				$results = array("error_text" => $e->getMessage());
-				http_response_code(400);
-			}	
+			$results = array("error_text" => "Invalid phone number.");
+			http_response_code(400);
 		}
+	}
+	else
+	{
+		$results = array("error_text" => "No phone number given");
+		$has_error = true;
+	}
+
+	// username
+	if(array_key_exists("username", $vars))
+	{
+		$username = $vars["username"];
+	}
+	else
+	{
+		$results = array("error_text" => "No username given");
+		$has_error = true;
+	}
+
+	// password
+	if(array_key_exists("password", $vars))
+	{
+		$password = $vars["password"];
+	}
+	else
+	{
+		$results = array("error_text" => "No password given");
+		$has_error = true;
+	}
+
+	if(!$has_error)
+	{
+		//unique username
+		//if(username_exists($db, ))
+		//$check_results = username_exists($db, $username);
+		$username_check_sql = "select count(*) from player where username = ?";
+		$statement_check_username = $db->prepare($username_check_sql);
+		$statement_check_username->execute([$username]);
+		$check_username_results_array = $statement_check_username->fetch(PDO::FETCH_ASSOC);
+		$check_username_results = intval($check_username_results_array["count"]);
+
+		//if the username is already exist, error text.
+		if($check_username_results !== 0)
+		{
+			$results = array("error_text" => "This username has been used.");
+			http_response_code(400);
+		}
+
+		try
+		{
+			$db->beginTransaction();
+
+			$highest_rank_sql = "select max(rank) from player";
+			$statement_highest_rank = $db->prepare($highest_rank_sql);
+			$statement_highest_rank->execute();
+			$highest_rank_result = $statement_highest_rank->fetch(PDO::FETCH_ASSOC);
+
+			$current_rank = $highest_rank_result["max"] + 1;
+
+
+			$sql = "insert into player (name, email, rank, phone, username, password) values (?, ?, ?, ?, ?, ?)";
+			$statement = $db->prepare($sql);
+			$statement->execute([$name, $email, $current_rank, $phone, $username, password_hash($password, PASSWORD_DEFAULT)]);
+			//password_hash($password)
+			$db->commit();
+
+			$results = array("error_text" => "");
+			http_response_code(202);
+		}
+		catch(PDOException $e)
+		{
+			$results = array("error_text" => $e->getMessage());
+			http_response_code(400);
+		}	
 	}
 	else
 	{
 		$results = array("error_text" => "Missing input(s).");
 		http_response_code(400);
 	}
-
-	
-	
 	//$results = array("resource" => "player", "method" => "POST", "request_vars" => $vars);
 }
 
+//updates the player with the given parameters.
 elseif($request->isPut())
 {
+	$username = $var["username"];// use which parameter as start?
+	//$check_results = username_exists($db, $username);
+	//check email is good. check phone# is good.(could same?4) check name(need exist). check rank is reasonable?
+	// if(!check_results)
+	// {
+	// 	$results = array("error_text" => "Username not found.");
+	// }
+	// else
+	// {
+
+	// }
+
 	$results = array("resource" => "player", "method" => "PUT", "request_vars" => $vars);
 }
 
@@ -182,5 +247,18 @@ elseif($request->isDelete())
 {
 	$results = array("resource" => "player", "method" => "DELETE", "request_vars" => $vars);
 }
+
+// function username_exists($db, $username)
+// {
+// 	$exists = false;
+// 	$username_check_sql = "select count(*) from player where username = ?";
+// 	$statement_check_username = $db->prepare($username_check_sql);
+// 	$statement_check_username->execute([$username]);
+// 	$check_username_results_array = $statement_check_username->fetch(PDO::FETCH_ASSOC);
+// 	$check_username_results = intval($check_username_results_array["count"]) == 1;
+
+// 	return exists;
+// }
+
 echo(json_encode($results));
 ?>
